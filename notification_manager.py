51574c7c3f2a06228a1d7fd838da27909f2b4a22
unknown <emanuele.gallone@mail.polimi.it>
2020-05-14 17:12:19 +0200

    conversion to object

--- notification_manager.py
+++ notification_manager.py
@@ -1,33 +1,35 @@
 """
 Copyright (c) Emanuele Gallone 05-2020.
 Author Emanuele Gallone
+
 """
 
 import mail_sender_service
 import telegram_bot_service
 from config_manager import ConfigManager
 
-config_manager = ConfigManager()
 
+class NotificationManager(object):
 
-def notify(msg="DEBUG FROM NOTIFICATION MANAGER!"):
-    _send_mail(msg)
-    _broadcast_alarm(msg)
+    def __init__(self):
+        self.config_manager = ConfigManager()
 
+    def notify(self, msg="DEBUG FROM NOTIFICATION MANAGER!"):
+        self._send_mail(msg)
+        self._broadcast_alarm(msg)
 
-def _send_mail(msg):
-    send_email_flag = config_manager.get_notification_params()['Send_email']
+    def _send_mail(self, msg):
+        send_email_flag = self.config_manager.get_notification_params()['Send_email']
 
-    if send_email_flag:
-        mail_sender_service.send_mail(msg)
+        if send_email_flag:
+            mail_sender_service.send_mail(msg)
 
+    def _broadcast_alarm(self, msg):
+        send_message_flag = self.config_manager.get_notification_params()['Send_message']
 
-def _broadcast_alarm(msg):
-    send_message_flag = config_manager.get_notification_params()['Send_message']
-
-    if send_message_flag:
-        telegram_bot_service.send_to_bot_group(msg)
+        if send_message_flag:
+            telegram_bot_service.send_to_bot_group(msg)
 
 
 if __name__ == '__main__':
-    notify()
+    notification_m = NotificationManager()
