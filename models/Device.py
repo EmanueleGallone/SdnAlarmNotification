3625b2aa20a753e3ccc890fe899833f2841dd2c9
unknown <emanuele.gallone@mail.polimi.it>
2020-05-15 14:51:46 +0200

    removed Device.py

--- models/Device.py
+++
@@ -1,9 +0,0 @@
-
-
-class Device(object):
-    def __init__(self, ip, netconf_rate, netconf_port, user, password):
-        self.ip = ip
-        self.netconf_rate = netconf_rate
-        self.netconf_port = netconf_port
-        self.user = user
-        self.password = password
