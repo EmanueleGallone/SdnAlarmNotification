4e8ce070e4e715100c8d53c007d66a4eb18460ef
unknown <emanuele.gallone@mail.polimi.it>
2020-05-15 14:11:44 +0200

    added model of device

---
+++ models/Device.py
@@ -0,0 +1,9 @@
+
+
+class Device(object):
+    def __init__(self, ip, netconf_rate, netconf_port, user, password):
+        self.ip = ip
+        self.netconf_rate = netconf_rate
+        self.netconf_port = netconf_port
+        self.user = user
+        self.password = password
